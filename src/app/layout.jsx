a02e118bd0cfa524768a3ca32b2b00d1049f3fa9
Benjamin Phillips <benjamingt537@gmail.com>
2023-08-10 12:42:17 -0400

    addded home button

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,8 @@
+"use client"
+
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { useRouter } from "next/navigation"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,9 +12,14 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }) {
+    const { push } = useRouter()
+
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <button className='absolute top-0 left-0 bg-white border border-black rounded-sm px-2 py-1' onClick={() => push("/")}>HOME</button>
+                {children}
+            </body>
         </html>
     )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     const { push } = useRouter()
 
     return (
-        <main>
+        <main className="w-screen h-screen grid place-items-center">
             <button onClick={() => push("screen-tearing")} className="px-2 py-1 bg-white border border-black">Screen Tearing Test</button>
             <button onClick={() => push("black-light")} className="px-2 py-1 bg-white border border-black">Black light Test</button>
             <button onClick={() => push("color-ranges")} className="px-2 py-1 bg-white border border-black">Color Ranges</button>
